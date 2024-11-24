d08ff9f2d4f2c4282bb45df9624a3574af4bad3b
Idris Azim <edrisazim25@gmail.com>
2024-11-23 22:22:19 -0800

    Update MainActivity.kt

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -7,7 +7,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.weatherapp.databinding.ActivityMainBinding
 
-var currentTemp = 0
+val API: String = "https://collectapi.com/tr/"
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
